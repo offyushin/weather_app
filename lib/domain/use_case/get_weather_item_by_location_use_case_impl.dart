cc3cb18a9ef4b40ff737dea448d6424d1362b649
offyushin <offyushin@naver.com>
2024-01-10 21:14:15 +0900

    refactor: get_weather_item_by_location_use_case_impl 의존성 제거

--- lib/domain/use_case/get_weather_item_by_location_use_case_impl.dart
+++ lib/domain/use_case/get_weather_item_by_location_use_case_impl.dart
@@ -1,11 +1,15 @@
 import 'package:weather_app/data/model/weather_item.dart';
-import 'package:weather_app/data/repository/weather_repository_impl.dart';
+import 'package:weather_app/domain/repository/weather_repository.dart';
 import 'package:weather_app/domain/use_case/get_weather_item_by_location_use_case.dart';
 
-class GetWeatherItemByLocationUseCaseImpl implements GetWeatherItemByLocationUseCase {
+class GetWeatherItemByLocationUseCaseImpl
+    implements GetWeatherItemByLocationUseCase {
+  WeatherRepository repository;
+
+  GetWeatherItemByLocationUseCaseImpl(this.repository);
+
   @override
-   Future<WeatherItem> execute() async {
-    final repository = WeatherRepositoryImpl();
+  Future<WeatherItem> execute() async {
     final weatherItem = await repository.getWeatherItem(37.55, 127);
 
     return weatherItem;
