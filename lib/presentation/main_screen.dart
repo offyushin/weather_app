89ecf9f38e885c378c447646ad4459f8fabfa2ad
offyushin <offyushin@naver.com>
2024-01-10 22:43:49 +0900

    refactor: 위젯 연결? 추가? UI 뼈대

--- lib/presentation/main_screen.dart
+++ lib/presentation/main_screen.dart
@@ -1,25 +1,30 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:weather_app/presentation/widgets/current_temp_text_widget.dart';
 import 'package:weather_app/presentation/widgets/lat_lng_text_widget.dart';
+
+import 'widgets/weather_icon_widget.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SizedBox(
-        width: double.infinity,
-        height: double.infinity,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            LatLngTextWidget(),
-            // WeatherIconWidget(),
-            // CurrentTempTextWidget(),
-          ],
+    return SafeArea(
+      child: Scaffold(
+        body: SizedBox(
+          width: double.infinity,
+          height: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              LatLngTextWidget(),
+              WeatherIconWidget(),
+              CurrentTempTextWidget(),
+            ],
+          ),
         ),
       ),
     );
